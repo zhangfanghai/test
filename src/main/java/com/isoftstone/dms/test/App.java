05040689beb7982271be1822833b710a764c73c2
ZhangFanghai <zhangfang9276@163.com>
2017-03-09 21:02:57 +0800

    测试

--- src/main/java/com/isoftstone/dms/test/App.java
+++ src/main/java/com/isoftstone/dms/test/App.java
@@ -4,12 +4,12 @@ package com.isoftstone.dms.test;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-		
-		
-    }
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		//张方海测试
+	}
 }
